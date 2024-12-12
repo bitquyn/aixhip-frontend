0fddc57827375089b9a090a63bde937f695bd48b
bitquyn <bitquyn@gmail.com>
2024-12-12 14:48:02 +0700

    theme toggler

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -1,12 +1,12 @@
 "use client"
 
 import { LucideIcon } from "lucide-react"
-
+import { ThemeToggle } from "./theme-toggle"
 import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-} from "@/components/ui/sidebar"
+} from "./ui/sidebar"
 
 interface NavMainProps {
   items: {
@@ -23,28 +23,33 @@ interface NavMainProps {
 
 export function NavMain({ items }: NavMainProps) {
   return (
-    <SidebarMenu>
-      {items.map((item) => (
-        <SidebarMenuItem key={item.title}>
-          <SidebarMenuButton asChild data-active={item.isActive}>
-            <a href={item.url}>
-              <item.icon className="size-4" />
-              {item.title}
-            </a>
-          </SidebarMenuButton>
-          {item.items?.length ? (
-            <SidebarMenu>
-              {item.items.map((subItem) => (
-                <SidebarMenuItem key={subItem.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={subItem.url}>{subItem.title}</a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          ) : null}
-        </SidebarMenuItem>
-      ))}
-    </SidebarMenu>
+    <div className="flex flex-col gap-4">
+      <SidebarMenu>
+        {items.map((item) => (
+          <SidebarMenuItem key={item.title}>
+            <SidebarMenuButton asChild data-active={item.isActive}>
+              <a href={item.url}>
+                <item.icon className="size-4" />
+                {item.title}
+              </a>
+            </SidebarMenuButton>
+            {item.items?.length ? (
+              <SidebarMenu>
+                {item.items.map((subItem) => (
+                  <SidebarMenuItem key={subItem.title}>
+                    <SidebarMenuButton asChild>
+                      <a href={subItem.url}>{subItem.title}</a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            ) : null}
+          </SidebarMenuItem>
+        ))}
+      </SidebarMenu>
+      <div className="px-4">
+        <ThemeToggle />
+      </div>
+    </div>
   )
 }
